263b8170f7c4f85e3de2f2c06326e1da7ff5183b
you <you@example.com>
2024-06-10 15:28:40 +0700

    vdfv

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ resource "google_storage_bucket" "my-bucket" {
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
+  project                  = "gitops-project-424508"
 }
