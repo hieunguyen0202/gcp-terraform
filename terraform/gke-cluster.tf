2b03b451dd13136c297f55e8095d30aaa657d30b
you <you@example.com>
2024-06-10 18:20:11 +0700

    xsx

--- terraform/gke-cluster.tf
+++ terraform/gke-cluster.tf
@@ -9,18 +9,18 @@ resource "google_container_cluster" "default" {
   subnetwork = google_compute_subnetwork.default.id
 
   ip_allocation_policy {
-    stack_type                    = "IPV4_IPV6"
-    services_secondary_range_name = google_compute_subnetwork.default.secondary_ip_range[0].range_name
-    cluster_secondary_range_name  = google_compute_subnetwork.default.secondary_ip_range[1].range_name
+    # Use default stack type (IPv4)
+    cluster_secondary_range_name  = google_compute_subnetwork.default.secondary_ip_range[0].range_name
+    services_secondary_range_name = google_compute_subnetwork.default.secondary_ip_range[1].range_name
   }
 
-  # Specify an initial node count greater than zero
+  # Set initial node count
   initial_node_count = 1
 
-  # Set `deletion_protection` to `true` will ensure that one cannot
-  # accidentally delete this instance by use of Terraform.
+  # Set deletion_protection
   deletion_protection = false
 }
+
 
 
 
