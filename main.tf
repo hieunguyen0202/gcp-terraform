c1d4f8b1d542a5357ebe5958c4fed54183971e3a
you <you@example.com>
2024-06-10 15:10:46 +0700

    123

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "google_storage_bucket" "my-bucket" {
+  name          = "no-public-access-bucket-m"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
