e0dca435869faac2254d5f072fcbf7b772d73a25
you <you@example.com>
2024-06-10 18:12:49 +0700

    dvfvf

--- terraform/gke-cluster.tf
+++ terraform/gke-cluster.tf
@@ -1,97 +1,119 @@
+resource "google_container_cluster" "default" {
+  name = "gke-cluster"
+
+  location                 = "us-central1"
+  enable_autopilot         = false
+  enable_l4_ilb_subsetting = true
+
+  network    = google_compute_network.default.id
+  subnetwork = google_compute_subnetwork.default.id
+
+  ip_allocation_policy {
+    stack_type                    = "IPV4_IPV6"
+    services_secondary_range_name = google_compute_subnetwork.default.secondary_ip_range[0].range_name
+    cluster_secondary_range_name  = google_compute_subnetwork.default.secondary_ip_range[1].range_name
+  }
+
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
+}
+
+
 
 
 # google_client_config and kubernetes provider must be explicitly specified like the following.
 
-data "google_client_config" "default" {}
+# data "google_client_config" "default" {}
 
-provider "kubernetes" {
-  host                   = "https://${module.gke.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-}
+# provider "kubernetes" {
+#   host                   = "https://${module.gke.endpoint}"
+#   token                  = data.google_client_config.default.access_token
+#   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+# }
 
-module "gke" {
-  source                     = "terraform-google-modules/kubernetes-engine/google"
-  project_id                 = "gitops-project-424508"
-  name                       = "gke-cluster"
-  region                     = "us-central1"
-  zones                      = ["us-central1-c"]
-  network                    = module.vpc.network_self_link 
-  subnetwork                 = "subnet-01"
-  http_load_balancing        = false
-  network_policy             = false
-  horizontal_pod_autoscaling = true
-  filestore_csi_driver       = false
-  ip_range_pods              = "subnet-01-secondary-01"
-  ip_range_services          = "subnet-01-secondary-02"
+# module "gke" {
+#   source                     = "terraform-google-modules/kubernetes-engine/google"
+#   project_id                 = "gitops-project-424508"
+#   name                       = "gke-cluster"
+#   region                     = "us-central1"
+#   zones                      = ["us-central1-c"]
+#   network                    = module.vpc.network_self_link 
+#   subnetwork                 = "subnet-01"
+#   http_load_balancing        = false
+#   network_policy             = false
+#   horizontal_pod_autoscaling = true
+#   filestore_csi_driver       = false
+#   ip_range_pods              = "subnet-01-secondary-01"
+#   ip_range_services          = "subnet-01-secondary-02"
 
 
-  node_pools = [
-    {
-      name                      = "default-node-pool"
-      machine_type              = "e2-medium"
-      node_locations            = "us-central1-c"
-      min_count                 = 1
-      max_count                 = 3
-      local_ssd_count           = 0
-      spot                      = false
-      disk_size_gb              = 30
-      disk_type                 = "pd-standard"
-      image_type                = "COS_CONTAINERD"
-      enable_gcfs               = false
-      enable_gvnic              = false
-      logging_variant           = "DEFAULT"
-      auto_repair               = true
-      auto_upgrade              = true
-      service_account           = "gitops-sa@gitops-project-424508.iam.gserviceaccount.com"
-      preemptible               = false
-      initial_node_count        = 3
-    },
-  ]
+#   node_pools = [
+#     {
+#       name                      = "default-node-pool"
+#       machine_type              = "e2-medium"
+#       node_locations            = "us-central1-c"
+#       min_count                 = 1
+#       max_count                 = 3
+#       local_ssd_count           = 0
+#       spot                      = false
+#       disk_size_gb              = 30
+#       disk_type                 = "pd-standard"
+#       image_type                = "COS_CONTAINERD"
+#       enable_gcfs               = false
+#       enable_gvnic              = false
+#       logging_variant           = "DEFAULT"
+#       auto_repair               = true
+#       auto_upgrade              = true
+#       service_account           = "gitops-sa@gitops-project-424508.iam.gserviceaccount.com"
+#       preemptible               = false
+#       initial_node_count        = 3
+#     },
+#   ]
 
-  node_pools_oauth_scopes = {
-    all = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
+#   node_pools_oauth_scopes = {
+#     all = [
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#     ]
+#   }
 
-  node_pools_labels = {
-    all = {}
+#   node_pools_labels = {
+#     all = {}
 
-    default-node-pool = {
-      default-node-pool = true
-    }
-  }
+#     default-node-pool = {
+#       default-node-pool = true
+#     }
+#   }
 
-  node_pools_metadata = {
-    all = {}
+#   node_pools_metadata = {
+#     all = {}
 
-    default-node-pool = {
-      node-pool-metadata-custom-value = "my-node-pool"
-    }
-  }
+#     default-node-pool = {
+#       node-pool-metadata-custom-value = "my-node-pool"
+#     }
+#   }
 
-  node_pools_taints = {
-    all = []
+#   node_pools_taints = {
+#     all = []
 
-    default-node-pool = [
-      {
-        key    = "default-node-pool"
-        value  = true
-        effect = "PREFER_NO_SCHEDULE"
-      },
-    ]
-  }
+#     default-node-pool = [
+#       {
+#         key    = "default-node-pool"
+#         value  = true
+#         effect = "PREFER_NO_SCHEDULE"
+#       },
+#     ]
+#   }
 
-  node_pools_tags = {
-    all = []
+#   node_pools_tags = {
+#     all = []
 
-    default-node-pool = [
-      "default-node-pool",
-    ]
-  }
-}
+#     default-node-pool = [
+#       "default-node-pool",
+#     ]
+#   }
+# }
 
 
 
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,69 +1,98 @@
+resource "google_compute_network" "default" {
+  name = "example-network"
 
+  auto_create_subnetworks  = false
+  enable_ula_internal_ipv6 = true
+}
 
-module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "~> 9.1"
+resource "google_compute_subnetwork" "default" {
+  name = "example-subnetwork"
 
-  project_id   = "gitops-project-424508"
-  network_name = "gitops-vpc"
-  routing_mode = "GLOBAL"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
 
-  subnets = [
-    {
-      subnet_name           = "subnet-01"
-      subnet_ip             = "10.10.10.0/24"
-      subnet_region         = "us-central1"
-    },
-    {
-      subnet_name           = "subnet-02"
-      subnet_ip             = "10.10.20.0/24"
-      subnet_region         = "us-central1"
-      subnet_private_access = "true"
-      subnet_flow_logs      = "true"
-      description           = "This subnet has a description"
-    },
-    {
-      subnet_name               = "subnet-03"
-      subnet_ip                 = "10.10.30.0/24"
-      subnet_region             = "us-central1"
-      subnet_flow_logs          = "true"
-      subnet_flow_logs_interval = "INTERVAL_10_MIN"
-      subnet_flow_logs_sampling = 0.7
-      subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-    }
-  ]
+  stack_type       = "IPV4_IPV6"
+  ipv6_access_type = "INTERNAL" # Change to "EXTERNAL" if creating an external loadbalancer
 
-  secondary_ranges = {
-    subnet-01 = [
-      {
-        range_name    = "subnet-01-secondary-01"
-        ip_cidr_range = "192.168.64.0/24"
-      },
-      {
-        range_name    = "subnet-01-secondary-02"
-        ip_cidr_range = "192.168.74.0/24"
-      }
-    ]
+  network = google_compute_network.default.id
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.0.0/24"
   }
 
-  routes = [
-    {
-      name                   = "egress-internet"
-      description            = "route through IGW to access internet"
-      destination_range      = "0.0.0.0/0"
-      tags                   = "egress-inet"
-      next_hop_internet      = "true"
-    },
-    {
-      name                   = "app-proxy"
-      description            = "route through proxy to reach app"
-      destination_range      = "10.50.10.0/24"
-      tags                   = "app-proxy"
-      next_hop_instance      = "app-proxy-instance"
-      next_hop_instance_zone = "us-central1-c"
-    },
-  ]
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.1.0/24"
+  }
 }
+
+
+
+
+# module "vpc" {
+#   source  = "terraform-google-modules/network/google"
+#   version = "~> 9.1"
+
+#   project_id   = "gitops-project-424508"
+#   network_name = "gitops-vpc"
+#   routing_mode = "GLOBAL"
+
+#   subnets = [
+#     {
+#       subnet_name           = "subnet-01"
+#       subnet_ip             = "10.10.10.0/24"
+#       subnet_region         = "us-central1"
+#     },
+#     {
+#       subnet_name           = "subnet-02"
+#       subnet_ip             = "10.10.20.0/24"
+#       subnet_region         = "us-central1"
+#       subnet_private_access = "true"
+#       subnet_flow_logs      = "true"
+#       description           = "This subnet has a description"
+#     },
+#     {
+#       subnet_name               = "subnet-03"
+#       subnet_ip                 = "10.10.30.0/24"
+#       subnet_region             = "us-central1"
+#       subnet_flow_logs          = "true"
+#       subnet_flow_logs_interval = "INTERVAL_10_MIN"
+#       subnet_flow_logs_sampling = 0.7
+#       subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+#     }
+#   ]
+
+#   secondary_ranges = {
+#     subnet-01 = [
+#       {
+#         range_name    = "subnet-01-secondary-01"
+#         ip_cidr_range = "192.168.64.0/24"
+#       },
+#       {
+#         range_name    = "subnet-01-secondary-02"
+#         ip_cidr_range = "192.168.74.0/24"
+#       }
+#     ]
+#   }
+
+#   routes = [
+#     {
+#       name                   = "egress-internet"
+#       description            = "route through IGW to access internet"
+#       destination_range      = "0.0.0.0/0"
+#       tags                   = "egress-inet"
+#       next_hop_internet      = "true"
+#     },
+#     {
+#       name                   = "app-proxy"
+#       description            = "route through proxy to reach app"
+#       destination_range      = "10.50.10.0/24"
+#       tags                   = "app-proxy"
+#       next_hop_instance      = "app-proxy-instance"
+#       next_hop_instance_zone = "us-central1-c"
+#     },
+#   ]
+# }
 # resource "google_compute_subnetwork" "existing_subnetwork" {
 #   name          = "gke-subnetwork"
 #   ip_cidr_range = "10.0.0.0/20"
